f9b0d9521cbb37af370b0878808d5f4257dbc71d
BenWalls <ruben.walangare@gmail.com>
2021-11-30 23:41:08 +0700

    Add comment to 02_repeatString

--- odin/javascript-excercises/02_repeatString.js
+++ odin/javascript-excercises/02_repeatString.js
@@ -9,3 +9,15 @@ const repeatString = function(string, num) {
     return (num < 0) ? 'ERROR' : result;
 };
 console.log(repeatString('', 15))
+
+/*
+At line 5, I put empty string so that it will add another string, it will be
+useful since I will add another string and you can see how it is relevant
+at line 7.
+
+At line 7, it is how I add string after string and of course I use loop to
+make it repeat until the condition met.
+
+At line 9, I think it is more efficient to use the ternary condition. Sleek
+and much more shorter than the if else in this case.
+*/
